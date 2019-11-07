df79805e5b2d21a45333728297b60e5de1f6a214
hmharvey <hmharvey@github.com>
2019-11-06 17:15:06 -0800

    test push

--- lib/cherry_pick_pr.rb
+++ lib/cherry_pick_pr.rb
@@ -11,4 +11,5 @@ if ENV["GITHUB_EVENT_NAME"] == "pull_request"
     `git config user.email "#{commit["commit"]["author"]["email"]}"`
     `git config user.name "#{commit["commit"]["author"]["name"]}"`
     end
+`git push`
 end
