5ffca9dd6542cf76e68f392178a8bfbcaf26c7d0
hmharvey <hmharvey@github.com>
2019-11-06 16:33:02 -0800

    time to git cherry pick

--- lib/cherry_pick_pr.rb
+++ lib/cherry_pick_pr.rb
@@ -5,6 +5,8 @@ require "open-uri"
 
 if ENV["GITHUB_EVENT_NAME"] == "pull_request"
   payload = JSON.parse(File.read(ENV["GITHUB_EVENT_PATH"]))
-  commits = JSON.parse(open(payload["pull_request"]["commits_url"]))
-  commits.each { |commit| puts commit["sha"] }
+  commits = JSON.parse(open(payload["pull_request"]["commits_url"]).read)
+  commits.each do |commit|
+    `git cherry-pick #{commit["sha"]}`
+    end
 end
