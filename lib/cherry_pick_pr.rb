8c8635cb941245d3ff3e87153c7c320aa4d7a553
hmharvey <hmharvey@github.com>
2019-11-06 16:10:20 -0800

    Test opening a webpage file

--- lib/cherry_pick_pr.rb
+++ lib/cherry_pick_pr.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 
 require "json"
+require "open-uri"
 
 if ENV["GITHUB_EVENT_NAME"] == "pull_request"
   payload = JSON.parse(File.read(ENV["GITHUB_EVENT_PATH"]))
-  commits = JSON.parse(File.read(payload["pull_request"]["commits_url"]))
+  commits = JSON.parse(open(payload["pull_request"]["commits_url"]))
   commits.each { |commit| puts commit["sha"] }
 end
