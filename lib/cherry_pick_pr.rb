f1b4e4ff49a3d6f3b33a564621528218c5c27e80
hmharvey <hmharvey@github.com>
2019-11-07 09:32:32 -0800

    git config first

--- lib/cherry_pick_pr.rb
+++ lib/cherry_pick_pr.rb
@@ -7,9 +7,9 @@ if ENV["GITHUB_EVENT_NAME"] == "pull_request"
   payload = JSON.parse(File.read(ENV["GITHUB_EVENT_PATH"]))
   commits = JSON.parse(open(payload["pull_request"]["commits_url"]).read)
   commits.each do |commit|
-    `git cherry-pick #{commit["sha"]}`
     `git config user.email "#{commit["commit"]["author"]["email"]}"`
     `git config user.name "#{commit["commit"]["author"]["name"]}"`
+    `git cherry-pick #{commit["sha"]}`
     end
 `git push`
 end
