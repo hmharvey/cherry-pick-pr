8baf843538ee4935498ddc386939ecd7965e9bf7
hmharvey <hmharvey@github.com>
2019-11-06 16:45:45 -0800

    syntax error

--- lib/cherry_pick_pr.rb
+++ lib/cherry_pick_pr.rb
@@ -8,7 +8,7 @@ if ENV["GITHUB_EVENT_NAME"] == "pull_request"
   commits = JSON.parse(open(payload["pull_request"]["commits_url"]).read)
   commits.each do |commit|
     `git cherry-pick #{commit["sha"]}`
-    `git config user.email "#{commit["commit"]["author"]["email"]}"
-    `git config user.name "#{commit["commit"]["author"]["name"]}"
+    `git config user.email "#{commit["commit"]["author"]["email"]}"`
+    `git config user.name "#{commit["commit"]["author"]["name"]}"`
     end
 end
