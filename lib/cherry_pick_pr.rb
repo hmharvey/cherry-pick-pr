a8c4bb2fb577b5a69d898a907c6fecf03d3e9b4c
hmharvey <hmharvey@github.com>
2019-11-08 11:18:31 -0800

    More debugging
    Co-Authored-By: Juan E. Pemberthy <jpemberthy@users.noreply.github.com>

--- lib/cherry_pick_pr.rb
+++ lib/cherry_pick_pr.rb
@@ -6,29 +6,35 @@ require "securerandom"
 
 if ENV["GITHUB_EVENT_NAME"] == "pull_request"
   payload = JSON.parse(File.read(ENV["GITHUB_EVENT_PATH"]))
-  commits = JSON.parse(open(payload["pull_request"]["commits_url"]).read)
 
   if payload["pull_request"]["head"]["repo"]["fork"]
-    `git remote add forked-branch "#{payload["pull_request"]["head"]["repo"]["clone_url"]}"`
-    `git fetch forked-branch`
-    puts "forked-branch"
+    `git remote add forked-repo "#{payload["pull_request"]["head"]["repo"]["clone_url"]}"`
+    `git fetch forked-repo`
+    puts "forked-repo"
     puts "#{payload["pull_request"]["head"]["repo"]["clone_url"]}"
 
-    `git remote add base-branch "#{payload["pull_request"]["base"]["repo"]["clone_url"]}"`
-    `git fetch base-branch`
-    puts "base-branch"
-    puts "#{payload["pull_request"]["base"]["repo"]["clone_url"]}"
+#     `git remote add base-repo "#{payload["pull_request"]["base"]["repo"]["clone_url"]}"`
+#     `git fetch base-repo`
+#     puts "base-repo"
+#     puts "#{payload["pull_request"]["base"]["repo"]["clone_url"]}"
   end
   
   branch_name = "cherry-pick-#{SecureRandom.hex(10)}"
+  puts `git branch`
+  `git checkout stable-branch`
   `git checkout -b "#{branch_name}"` 
 
+  commits = JSON.parse(open(payload["pull_request"]["commits_url"]).read)
   commits.each do |commit|
-    `git config user.email "#{commit["commit"]["author"]["email"]}"`
-    `git config user.name "#{commit["commit"]["author"]["name"]}"`
+#    `git config user.email "#{commit["commit"]["author"]["email"]}"`
+#    `git config user.name "#{commit["commit"]["author"]["name"]}"`
     `git cherry-pick "#{commit["sha"]}"`
     end
+
+  puts "before git push"
   `git push --set-upstream origin "#{branch_name}"`
-  `echo ::set-output name=branch_name::"#{branch_name}"`
+  puts "after git push"
+
+  # delete remote if fork
   puts `echo ::set-output name=branch_name::"#{branch_name}"`
 end
