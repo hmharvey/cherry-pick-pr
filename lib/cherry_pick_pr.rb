6fd173c45f6a0f356ec909bebf5fd9759bd90a65
hmharvey <hmharvey@github.com>
2019-11-08 11:49:08 -0800

    Check functionality wo commits
    Co-Authored-By: Juan E. Pemberthy <jpemberthy@users.noreply.github.com>

--- lib/cherry_pick_pr.rb
+++ lib/cherry_pick_pr.rb
@@ -12,23 +12,18 @@ if ENV["GITHUB_EVENT_NAME"] == "pull_request"
     `git fetch forked-repo`
     puts "forked-repo"
     puts "#{payload["pull_request"]["head"]["repo"]["clone_url"]}"
-
-#     `git remote add base-repo "#{payload["pull_request"]["base"]["repo"]["clone_url"]}"`
-#     `git fetch base-repo`
-#     puts "base-repo"
-#     puts "#{payload["pull_request"]["base"]["repo"]["clone_url"]}"
   end
   
   branch_name = "cherry-pick-#{SecureRandom.hex(10)}"
   puts `git status -b --porcelain`
   `git checkout -b "#{branch_name}"` 
 
-  commits = JSON.parse(open(payload["pull_request"]["commits_url"]).read)
-  commits.each do |commit|
-    `git config user.email "#{commit["commit"]["author"]["email"]}"`
-    `git config user.name "#{commit["commit"]["author"]["name"]}"`
-    `git cherry-pick "#{commit["sha"]}"`
-    end
+#   commits = JSON.parse(open(payload["pull_request"]["commits_url"]).read)
+#   commits.each do |commit|
+#     `git config user.email "#{commit["commit"]["author"]["email"]}"`
+#     `git config user.name "#{commit["commit"]["author"]["name"]}"`
+#     `git cherry-pick "#{commit["sha"]}"`
+#     end
 
   puts "before git push"
   `git push --set-upstream origin "#{branch_name}"`
