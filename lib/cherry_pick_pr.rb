177fe644b8f821214d3d30fa9e6a63866ce8306e
hmharvey <hmharvey@github.com>
2019-11-07 18:05:14 -0800

    Add forked repo support

--- lib/cherry_pick_pr.rb
+++ lib/cherry_pick_pr.rb
@@ -5,10 +5,17 @@ require "open-uri"
 require "securerandom"
 
 if ENV["GITHUB_EVENT_NAME"] == "pull_request"
-  branch_name = "cherry-pick-#{SecureRandom.hex(10)}"
-  `git checkout -b "#{branch_name}"` 
   payload = JSON.parse(File.read(ENV["GITHUB_EVENT_PATH"]))
   commits = JSON.parse(open(payload["pull_request"]["commits_url"]).read)
+
+  if payload["repository"]["fork"]
+    `git remote add forked-branch "#{payload["repository"]["clone_url"]}"`
+    `git fetch forked-branch`
+  end
+  
+  branch_name = "cherry-pick-#{SecureRandom.hex(10)}"
+  `git checkout -b "#{branch_name}"` 
+
   commits.each do |commit|
     `git config user.email "#{commit["commit"]["author"]["email"]}"`
     `git config user.name "#{commit["commit"]["author"]["name"]}"`
